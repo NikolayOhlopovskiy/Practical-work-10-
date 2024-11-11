704d277d80ec17964851a4ae2acce9b4320b5405
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2024-11-11 21:59:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание1 В одномерном массиве из 8 целых чисел с явной инициализацией обнулить значения трех элементов, начиная с индекса, равного 1.
+            //int[] omas = new int[8] { 1, 2, 3, 5, 7, 9, 11, 12, };
+            //Array.Clear(omas, 1, 3);
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]= {omas[i]}");
+            //}
+            //Console.ReadLine();
+
+            //Задание2 Изменить размер одномерного массива.
+            //int[] omas = new int[5] { 1, 2, 3, 4, 5 };
+            ////Array.Resize(ref omas, 9); //Увеличение массива
+            ////Array.Resize(ref omas, 3); //Уменьшение массива
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]= {omas[i]}");
+            //}
+            //Console.ReadLine();
+
+
+            //Здание3 Найти индекс первого отрицательного элемента одномерного массива, заполненного случайным образом в диапазоне от -5 до 5.
+            //int[] omas = new int[10];
+            //Random random = new Random();
+
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    omas[i] = random.Next(-5, 6);
+            //    Console.WriteLine($"omas[{i}] = {omas[i]}");
+            //}
+            //int negarive = Array.FindIndex(omas, index => index < 0);
+            //Console.WriteLine($"Индекс первого отрицательного элемента: {negarive} ");
+            //Console.ReadLine();
+        }
+    }
+}
